6dad4de4333184df3d8b68047f406a218578c9b2
putilovskaya <putilovskayaa@mail.ru>
2024-01-02 20:31:51 +0300

    add fixtures to test_arrs.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,24 @@
+import pytest
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], 0, "test") == "test"
+@pytest.mark.parametrize('array, index, default, expected', [
+    ([1, 2, 3], 1, 'test', 2),
+    ([], 0, 'test', 'test')
+])
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_get(array, index, default, expected):
+    assert arrs.get(array, index, default) == expected
+
+
+@pytest.mark.parametrize('coll, start, end, expected', [
+    ([1, 2, 3, 4], 1, 3, [2, 3]),
+    ([1, 2, 3], 1, None, [2, 3]),
+    ([], 1, 3, []),
+    ([1, 2, 3, 4], None, 2, [1, 2])
+])
+
+
+def test_slice(coll, start, end, expected):
+    assert arrs.my_slice(coll, start, end) == expected
